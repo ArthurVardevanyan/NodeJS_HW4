4c50e2fff54da4f94cdc0b5c847b6901e46988f1
Arthur <arthur@arthurvardevanyan.com>
2021-04-13 08:17:29 -0400

    Split Server and Socket Code

--- server.js
+++ server.js
@@ -10,58 +10,9 @@ app.use(Express.json());
 
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const xss = require('xss');
-
-const port = 8080;
-const Service = require('./services/service');
+require('./socket')(io);
 
 app.use(Express.static(`${__dirname}/public`));
-
-io.on('connection', (socket) => {
-  socket.on('typingInput', async (inputString) => {
-    const searchStrings = await Service.getStartsWith(inputString);
-    const sanitizedSearchStrings = [];
-    searchStrings.forEach((str) => {
-      sanitizedSearchStrings.push({ Name: xss(str.Name) });
-    });
-    socket.emit('typingInput', sanitizedSearchStrings);
-  });
-
-  socket.on('Input', async (inputString) => {
-    let input = '';
-    try {
-      input = await Service.post({ Name: xss(inputString) });
-    } catch (e) {
-      if (e.code !== 11000) {
-        throw e;
-      }
-    }
-    socket.emit(input);
-  });
-
-  socket.on('clear', async () => {
-    let input = '';
-    try {
-      input = await Service.deleteAll();
-    } catch (e) {
-      if (e.code !== 11000) {
-        throw e;
-      }
-    }
-    socket.emit(input);
-  });
-  socket.on('del', async (inputString) => {
-    let input = '';
-    try {
-      input = await Service.deleteString(xss(inputString));
-    } catch (e) {
-      if (e.code !== 11000) {
-        throw e;
-      }
-    }
-    socket.emit(input);
-  });
-});
 
 (async () => {
   await Mongoose.connect(process.env.MONGO_DB, {
@@ -70,7 +21,7 @@ io.on('connection', (socket) => {
     useFindAndModify: false,
     useCreateIndex: true,
   });
-  http.listen(port);
+  http.listen(8080);
 })();
 
 module.exports = app;
